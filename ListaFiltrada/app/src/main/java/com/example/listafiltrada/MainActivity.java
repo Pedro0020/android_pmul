ad6628cb06dfa3f5fe66654e3f69d0f0227c7ee8
Pedro <pedromaroga@gmail.com>
2023-11-25 20:00:34 +0100

    añado lista filtrada

--- ListaFiltrada/app/src/main/java/com/example/listafiltrada/MainActivity.java
+++ ListaFiltrada/app/src/main/java/com/example/listafiltrada/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.listafiltrada;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText filtro;
+    private Spinner contenedor;
+    private SpinnerAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
